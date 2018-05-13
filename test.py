2417fac5e3a91482ac43740abca00a945a8389ce
polarisZhao <zhaozhichao.ml@gmail.com>
2018-05-13 12:05:54 +0800

    add test script

---
+++ test.py
@@ -0,0 +1,12 @@
+from src.detector import detect_faces
+from utils.visualization_utils import show_bboxes
+from PIL import Image
+
+def main():
+    image = Image.open('images/test3.jpg')
+    bounding_boxes, landmarks = detect_faces(image)
+    image = show_bboxes(image, bounding_boxes, landmarks)
+    image.show()
+
+if __name__ == "__main__":
+    main()
